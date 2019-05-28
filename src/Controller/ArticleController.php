44919252031875d97b9e350937019d2c1f4d4081
alloy <alloy@syneforge.com>
2019-05-28 12:11:43 +0300

    Added twig files

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -26,5 +27,26 @@ class ArticleController
     public function test ( $testvar )
     {
         return new Response( "Here it is: $testvar" );
+    }
+
+    /**
+     * @Route("/show/{testvar}")
+     * @param $testvar
+     *
+     * @return Response
+     * @throws \LogicException
+     */
+    public function show ( $testvar )
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render( 'article/show.html.twig', [
+            'title' => ucwords( str_replace( '-', ' ', $testvar ) ),
+            'comments' => $comments,
+        ] );
     }
 }
